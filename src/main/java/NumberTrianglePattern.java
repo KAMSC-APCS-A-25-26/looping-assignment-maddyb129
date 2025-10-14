2cae3deecfb8f6a09dcac10d2d5f0a4847dd52b6
maddyb129 <maddyball814@gmail.com>
2025-10-14 13:18:18 -0400

    Update NumberTrianglePattern.java

--- src/main/java/NumberTrianglePattern.java
+++ src/main/java/NumberTrianglePattern.java
@@ -2,11 +2,26 @@ import java.util.Scanner;
 
 public class NumberTrianglePattern {
     public static void main(String[] args) {
-                
+
         // TODO: Step 1 - Get user input
-
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
         // TODO: Step 2 - Generate the number triangle pattern
-
+        if (n <= 0)
+        {
+            System.out.println("Please enter a positive integer.");
+        }
+        else
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
